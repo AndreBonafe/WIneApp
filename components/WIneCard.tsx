a7fab057c59e5e0088e1e265eb6e2aa45fe2fd5c
André Bonafé <andre.bonaf@gmail.com>
2022-06-30 20:25:45 -0300

    Componente de card de vinhos finalizado :wine_glass:

--- components/WIneCard.tsx
+++ components/WIneCard.tsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
 import { WineObj } from '../Interfaces/WineInterface'; 
 
 const WineCard = ({ wine }: { wine: WineObj }) => {
   return (
     <div>
+      <Image
+        src={wine.image}
+        alt={`${wine.name}-image`}
+        width={232.5}
+        height={350.5}
+      />
       <p>{wine.name}</p>
+      <p>
+        <span>{`R$${wine.price} `}</span>
+        <span>{`  ${wine.discount}%OFF!`}</span>
+      </p>
+      <p>
+        <span>SÓCIO WINE</span>
+        <span>{`  R$${wine.priceMember}`}</span>
+      </p>
+      <p>
+        <span>NÃO SÓCIO</span>
+        <span>{`  R$${wine.priceNonMember}`}</span>
+      </p>
+      <button type='button'>ADICIONAR</button>
     </div>
   );
 };
